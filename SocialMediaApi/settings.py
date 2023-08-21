fc559165d211b702294f8ca80e7b9a4c8de3b6b9
kostas2370 <96636678+kostas2370@users.noreply.github.com>
2023-08-21 03:20:50 +0300

    env test

--- SocialMediaApi/settings.py
+++ SocialMediaApi/settings.py
@@ -146,13 +146,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'test1234aqww@gmail.com'
-EMAIL_HOST_PASSWORD = 'rjmhuntxriwejbxb'
-EMAIL_PORT = 587
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
